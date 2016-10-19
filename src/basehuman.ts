787364ca83d8fd5e3eab5d20f890eaf442e0f2b1
Matt Dotson <mdotson@microsoft.com>
2016-10-18 17:25:52 -0700

    most unit tests passing

---
+++ src/basehuman.ts
@@ -0,0 +1,86 @@
+import fs = require('fs');
+import readline = require('readline');
+import path = require('path');
+
+const BASE_MIN : number = 2;
+const BASE_MAX : number = 10000;
+// see: http://www.unicode.org/reports/tr18/#Line_Boundaries
+const NEWLINE = /\r\n|[\n\v\f\r\x85\u2028\u2029]/g.compile();
+const MAX_SHIFTABLE = Math.pow(2,32) - 1;
+
+export interface IBaseHumanOptions {
+    language : string,
+    base : number,
+    seperator : string
+};
+
+
+export class BaseHuman {
+    private dict : string[] = [];
+    private opt : IBaseHumanOptions = {
+        language : "en",
+        base : 1024,
+        seperator : " "
+    };
+
+    private rs : fs.ReadStream;
+
+    /**
+     * constructor
+     */
+    constructor(options : IBaseHumanOptions) {
+        this.opt = Object.assign(this.opt, options);
+
+        //check language so we don't let someone open a bad file
+        if(!/^[a-z]{2}$/.test(this.opt.language)) {
+            throw new Error("invalid language identifier passed");
+        }
+
+        if(this.opt.base < BASE_MIN || this.opt.base > BASE_MAX) {
+            throw new Error(`base must be between ${BASE_MIN} and ${BASE_MAX}`);
+        }
+
+        this.dict = require(`./data/${this.opt.language}`);
+    }
+
+    /**
+     * encodes data 
+     */
+    public encode(data : number) : string {
+        let digits : string[] = [];
+        let n = data;
+
+        if(0 === data) {
+            return this.dict[0];
+        }
+
+        while(n > 0) {
+            let r = n % this.opt.base;
+            n = Math.floor(n / this.opt.base);
+            digits.push(this.dict[r]);
+        }
+
+        return digits.reverse().join(this.opt.seperator);
+    }
+
+    /**
+     * decodes data
+     */
+    public decode(encoded : string) {
+        let digits = encoded.split(this.opt.seperator);
+        let val : number = 0;
+        for(let dix in digits) {
+            let ix = this.dict.findIndex((val) => val === digits[dix]);
+            val = (val * this.opt.base) + ix;
+        }
+        
+        return val;
+    }
+
+    /**
+     * get/set the base
+     */
+    public base(b? : number) {
+        return this.opt.base;
+    }
+}
---
+++ test/basehuman-tests.js
@@ -0,0 +1,46 @@
+"use strict";
+const chai = require("chai");
+chai.should();
+
+const BaseHuman = require("../lib/basehuman").BaseHuman;
+const data = require("../lib/data/en");
+
+console.log('starting tests...', new Date().toISOString());
+
+describe("basehuman", ()=> {
+    let bases = [128, 1024, 2048, 4096, 10000];
+    bases.forEach(function(base) {
+        describe(`base-${base}`, ()=> {
+            let bh = new BaseHuman({ base : base });
+            describe("encode", () => {
+                it("should encode 0 as 'the'", ()=> {
+                    chai.assert.equal(bh.encode(0x0), 'the');
+                });
+                it("should encode -1 as ''", ()=> {
+                    chai.assert.equal(bh.encode(-1), '');
+                });
+            });
+
+            describe("decode", () => {
+                let levels = [0,1,2].map((p) => Math.pow(bh.base(),p));
+                let tests = levels.concat(levels.map((x)=> x-1), levels.map((x)=> x+1), levels.map((x)=>x+2));
+                tests.forEach((i) => {
+                    it(`should decode '${bh.encode(i)}' as ${i}`, ()=>{    
+                        //let n = Math.ceil(Math.random() * Number.MAX_VALUE);
+                        chai.assert.equal(bh.decode(bh.encode(i)), i);
+                    });
+                });
+
+                it(`should decode '${bh.encode(Number.MAX_SAFE_INTEGER)}' as ${Number.MAX_SAFE_INTEGER}`, ()=>{    
+                    //let n = Math.ceil(Math.random() * Number.MAX_VALUE);
+                    chai.assert.equal(bh.decode(bh.encode(Number.MAX_SAFE_INTEGER)), Number.MAX_SAFE_INTEGER);
+                });
+
+                it(`should decode '${bh.encode(Number.MAX_VALUE)}' as ${Number.MAX_VALUE}`, ()=>{    
+                    //let n = Math.ceil(Math.random() * Number.MAX_VALUE);
+                    chai.assert.equal(bh.decode(bh.encode(Number.MAX_VALUE)), Number.MAX_VALUE);
+                });
+            });
+        });
+    });
+});
